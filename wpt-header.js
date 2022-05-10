3faf43a9a7fc51a72e0c2a5bcde1c81607655cce
Scott Jehl <scottjehl@users.noreply.github.com>
2022-05-10 16:45:52 -0400

    first

---
+++ wpt-header.js
@@ -0,0 +1,63 @@
+class WptHeader extends HTMLElement {
+    constructor() {
+        super();
+        this._init = this._init.bind(this);
+        this._observer = new MutationObserver(this._init);
+    }
+    connectedCallback() {
+        if (this.children.length) {
+            this._init();
+        }
+        this._observer.observe(this, { childList: true });
+    }
+    _init() {
+        this.menubtn = this.querySelector(".wptheader_menubtn");
+        this.menu = this.menubtn.closest("details");
+        this.details = this.querySelectorAll("nav details");
+        let that = this;
+        window.addEventListener("resize", function() {
+            that.updateMenuAttrs();
+        })
+        this.updateMenuAttrs();
+        this.menuHovers();
+    }
+    menuHovers() {
+        let menuDisplay = getComputedStyle(this.menubtn).display;
+        let that = this;
+        this.details.forEach(function(detail) {
+            detail.addEventListener("mouseenter", function() {
+                if (menuDisplay === "none") {
+                    this.open = true;
+                }
+            });
+            detail.addEventListener("mouseleave", function() {
+                if (menuDisplay === "none") {
+                    this.open = false;
+                }
+            });
+            detail.addEventListener("click", function(e) {
+                if (menuDisplay === "none") {
+                    that.details.forEach(function(otherdeet) {
+                        if (otherdeet !== e.target) {
+                            otherdeet.open = false;
+                        }
+                    });
+                }
+            });
+        });
+    }
+    updateMenuAttrs() {
+        let menuDisplay = getComputedStyle(this.menubtn).display;
+        if (menuDisplay === "none") {
+            this.menu.open = true;
+        } else {
+            this.menu.open = false;
+        }
+
+    }
+
+}
+
+if ('customElements' in window) {
+    customElements.define('wpt-header', WptHeader);
+}
